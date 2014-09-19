a9fe9e259fa596aa24208e1fdb7b123aa7da62bc
Wutthichai <limplemoness@gmail.com>
2014-09-19 18:56:09 +0700

    set env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-    'local' => ['*.dev', gethostname()],
+    'local' => ['*.dev'], 
     'production' => ['*.com', '*.net', '*.org']
 ));
 
