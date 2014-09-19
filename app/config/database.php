b08d48c6ff2cffd6e5d643fc42f87a0336ca5a25
Wutthichai <limplemoness@gmail.com>
2014-09-19 16:43:41 +0700

    config database production

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'udeecondo',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'coolspace_ud',
+			'username'  => 'coolspace_ud',
+			'password'  => 'hyJzHGy1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
