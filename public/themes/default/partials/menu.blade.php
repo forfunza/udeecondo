c2b2041045297778ecfbfaab42ee093aef118dc1
Sukhuma <Oom@Sukhumas-iMac.local>
2014-09-22 00:00:19 +0700

    edit style project info page & edit main menu

--- public/themes/default/partials/menu.blade.php
+++ public/themes/default/partials/menu.blade.php
@@ -6,14 +6,14 @@
         <nav>
             <ul class="menu">
                 <li><a href="{{ action('HomeController@index') }}" class="{{ Request::segment(2) == '' ? 'active' : ''  }}">{{{ trans('menu.home') }}}</a></li>
-                <li><a class="{{ Request::segment(2) == 'project' ? 'active' : ''  }}">{{{ trans('menu.information') }}} <span class="caret"></span></a>
+                <li><a href="#" class="{{ Request::segment(2) == 'project' ? 'active' : ''  }}">{{{ trans('menu.information') }}} <span class="caret"></span></a>
                 <ul class="sub-menu">
                     <li><a href="{{ action('HomeController@detail') }}">{{{ trans('menu.detail') }}}</a></li>
                     <li><a href="{{ action('HomeController@concept') }}">{{{ trans('menu.concept') }}}</a></li>
                     <li><a href="{{ action('HomeController@facility') }}" class="last">{{{ trans('menu.facility') }}}</a></li>
                 </ul>
             </li>
-            <li><a class="{{ Request::segment(2) == 'plan' ? 'active' : ''  }}">{{{ trans('menu.plan') }}} <span class="caret"></span></a>
+            <li><a href="#" class="{{ Request::segment(2) == 'plan' ? 'active' : ''  }}">{{{ trans('menu.plan') }}} <span class="caret"></span></a>
             <ul class="sub-menu">
                 <li><a href="{{ action('HomeController@master') }}">{{{ trans('menu.project_plan') }}}</a></li>
                 <li><a href="{{ action('HomeController@floor') }}">{{{ trans('menu.floor_plan') }}}</a></li>
--- public/themes/default/views/information/detail.blade.php
+++ public/themes/default/views/information/detail.blade.php
@@ -1,29 +1,62 @@
 <div class="bg-project-info"><img src="{{ asset('themes/default/assets/images/info-img.jpg') }}" /></div>
-<div class="pi-content">ชื่อโครงการ :   {{ $project->first()->pivot->name }}
+<div class="pi-content"><span class="projectinfoLeft">ชื่อโครงการ :</span>   <span class="projectinfoRight"> {{ $project->first()->pivot->name }}</span>
 
-ทำเลที่ตั้ง   :   {{ $project->first()->pivot->address }} 
+<span class="projectinfoLeft">ทำเลที่ตั้ง   :</span>   <span class="projectinfoRight"> ตำบลโสธร อำเภอเมือง จ.ะเชิงเทราติดโรบินสัน ใกล้สนามบินสุวรรณภูมิ {{ $project->first()->pivot->address }} </span>
 
-จำนวน (ชั้น/ยูนิต) :
+<span class="projectinfoLeft infoau">จำนวน (ชั้น/ยูนิต) :</span>
+<span class="projectinfoRight nl">
+	<ul>
+    	<li>เฟส 1 :  อาคารชุดสูง 7 ชั้น  4  อาคาร จำนวน  316 ยูนิต</li>
+        <li>เฟส 2 :  อาคารชุดสูง 8 ชั้น  4  อาคาร จำนวน  607 ยูนิต</li>
+    </ul>
 @foreach ($units as $unit) 
 {{ $unit->pivot->name }} :  {{ $unit->pivot->building }} จำนวน  {{ $unit->pivot->unit }} ยูนิต
 @endforeach
-ราคาเริ่มต้น :   {{ $project->first()->price }}
+</span>
+<span class="projectinfoLeft">ราคาเริ่มต้น :</span>  <span class="projectinfoRight"> {{ $project->first()->price }}</span>
 
-รูปแบบห้องชุด :
+<span class="projectinfoLeft infoau">รูปแบบห้องชุด :</span>
+<span class="projectinfoRight nl ml">
+	<ul>
+    	<li> :  ห้องชุดขนาด 22.5  ตารางเมตร</li>
+        <li> :  ห้องชุดขนาด 24  ตารางเมตร </li>
+    </ul>
 @foreach ($rooms as $room) 
 :   {{ $room->pivot->name }} 
 @endforeach 
+</span>
 
-พื้นที่จอดรถ :
+<span class="projectinfoLeft infoau">พื้นที่จอดรถ :</span>
+<span class="projectinfoRight nl ml">
+<ul>
+    	<li> เฟส 1 :  120 คัน</li>
+        <li> เฟส 2 :  180 คัน </li>
+    </ul>
 @foreach ($units as $unit) 
 {{ $unit->pivot->name }} :  {{ $unit->parking }} คัน
 @endforeach
+</span>
 
-สิ่งอำนวยความสะดวก : 
+<span class="projectinfoLeft infoau">สิ่งอำนวยความสะดวก : </span>
+<span class="projectinfoRight nl ml">
+<ul>
+    	<li> สโมสร , สระว่ายน้ำ , ฟิตเนต , 
+                ร้านอาหาร , ร้านสะดวกซื้อ ,
+                สวนสาธารณะ , Joggin Track</li>
+    </ul>
 @foreach ($facilities as $facility) 
 {{ $facility->pivot->name }} 
 @endforeach 
+</span>
 
-ระบบรักษาความปลอดภัย  :  
+<span class="projectinfoLeft infoau">ระบบรักษาความปลอดภัย  : </span>
+<span class="projectinfoRight nl ml">
+<ul>
+    	<li>  ระบบรักษาความปลอดภัยด้วย รปภ.
+                24 ชั่วโมง  เข้า-ออกโครงการและ
+                ตัวอาคารชุด้วยระบบ Access Card
+                เพิ่มพื้นที่ส่วนตัวสำหรับคุณ </li>
+    </ul>
 {{ $project->first()->pivot->security }} 
+</span>
 </div>
