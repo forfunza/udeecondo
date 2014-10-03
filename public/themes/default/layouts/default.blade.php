cbb1c0fa52ebc2065a9049d9eb8471782c7656f6
Wutthichai <limplemoness@gmail.com>
2014-10-03 12:47:40 +0700

    fix style

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -6,7 +6,7 @@
 <!-- <![endif]-->
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>{{ Theme::get('title') }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     {{ Theme::asset()->styles() }}
