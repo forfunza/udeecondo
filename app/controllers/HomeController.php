57cb2baff213f1232e6bf535f90a2f02557a6b29
Wutthichai <limplemoness@gmail.com>
2014-10-03 12:10:21 +0700

    fix bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -128,7 +128,7 @@ class HomeController extends BaseController {
         });
     </script>
 ');
-		$floors = BuildingFloor::where('building_id',$id)->get();
+		$floors = BuildingFloor::where('building_id',$id)->orderBy('sort_order','desc')->get();
 		$building = Building::find($id);
         $view = array(
         	'floors' => $floors,
--- public/themes/default/views/plan/floor.blade.php
+++ public/themes/default/views/plan/floor.blade.php
@@ -2,23 +2,23 @@
 <span class="mtip">กรุณาเลือกอาคารที่ท่านต้องการดูรายละเอียดเพิ่มเติม</span>
 <div class="mtp">
     <span class="main-master"><img src="{{ asset('themes/default/assets/images/plan/masterplan.png') }}" /></span>
-    <a href="{{ action('HomeController@floor_detail',7) }}"><span class="floor-1 tooltipe" title="อาคาร ก้าวหน้า"></span></a>
-    <a href="{{ action('HomeController@floor_detail',8) }}"><span class="floor-2 tooltipe" title="อาคาร ขุมทรัพย์"></span></a>
-    <a href="{{ action('HomeController@floor_detail',9) }}"><span class="floor-3 tooltipe" title="อาคาร งอกงาม"></span></a>
-    <a href="{{ action('HomeController@floor_detail',10) }}"><span class="floor-4 tooltipe" title="อาคาร จงเจริญ"></span></a>
-    <a href="{{ action('HomeController@floor_detail',11) }}"><span class="floor-5 tooltipe" title="อาคาร ฉลองชัย"></span></a>
-    <a href="{{ action('HomeController@floor_detail',12) }}"><span class="floor-6 tooltipe" title="อาคาร ชัยชนะ"></span></a>
-    <a href="{{ action('HomeController@floor_detail',13) }}"><span class="floor-7 tooltipe" title="อาคาร ดวงดี"></span></a>
-    <a href="{{ action('HomeController@floor_detail',14) }}"><span class="floor-8 tooltipe" title="อาคาร ทองแท้"></span></a>
+    <a href="{{ action('HomeController@floor_detail',1) }}"><span class="floor-1 tooltipe" title="อาคาร ก้าวหน้า"></span></a>
+    <a href="{{ action('HomeController@floor_detail',2) }}"><span class="floor-2 tooltipe" title="อาคาร ขุมทรัพย์"></span></a>
+    <a href="{{ action('HomeController@floor_detail',3) }}"><span class="floor-3 tooltipe" title="อาคาร งอกงาม"></span></a>
+    <a href="{{ action('HomeController@floor_detail',4) }}"><span class="floor-4 tooltipe" title="อาคาร จงเจริญ"></span></a>
+    <a href="{{ action('HomeController@floor_detail',5) }}"><span class="floor-5 tooltipe" title="อาคาร ฉลองชัย"></span></a>
+    <a href="{{ action('HomeController@floor_detail',6) }}"><span class="floor-6 tooltipe" title="อาคาร ชัยชนะ"></span></a>
+    <a href="{{ action('HomeController@floor_detail',7) }}"><span class="floor-7 tooltipe" title="อาคาร ดวงดี"></span></a>
+    <a href="{{ action('HomeController@floor_detail',8) }}"><span class="floor-8 tooltipe" title="อาคาร ทองแท้"></span></a>
      <span class="arrm"><img src="{{ asset('themes/default/assets/images/arr-mtp.jpg') }}" /></span>
 </div>
 <div class="floor-mobi map-box">
-	<a href="{{ action('HomeController@floor_detail',7) }}">อาคาร ก้าวหน้า <img src="{{ asset('themes/default/assets/images/plan/floor-1.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',8) }}">อาคาร ขุมทรัพย์ <img src="{{ asset('themes/default/assets/images/plan/floor-2.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',9) }}">อาคาร งอกงาม <img src="{{ asset('themes/default/assets/images/plan/floor-3.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',10) }}">อาคาร จงเจริญ <img src="{{ asset('themes/default/assets/images/plan/floor-4.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',11) }}">อาคาร ฉลองชัย <img src="{{ asset('themes/default/assets/images/plan/floor-5.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',12) }}">อาคาร ชัยชนะ <img src="{{ asset('themes/default/assets/images/plan/floor-6.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',13) }}">อาคาร ดวงดี <img src="{{ asset('themes/default/assets/images/plan/floor-7.jpg') }}" ></a>
-    <a href="{{ action('HomeController@floor_detail',14) }}">อาคาร ทองแท้ <img src="{{ asset('themes/default/assets/images/plan/floor-8.jpg') }}" ></a>
+	<a href="{{ action('HomeController@floor_detail',1) }}">อาคาร ก้าวหน้า <img src="{{ asset('themes/default/assets/images/plan/floor-1.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',2) }}">อาคาร ขุมทรัพย์ <img src="{{ asset('themes/default/assets/images/plan/floor-2.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',3) }}">อาคาร งอกงาม <img src="{{ asset('themes/default/assets/images/plan/floor-3.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',4) }}">อาคาร จงเจริญ <img src="{{ asset('themes/default/assets/images/plan/floor-4.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',5) }}">อาคาร ฉลองชัย <img src="{{ asset('themes/default/assets/images/plan/floor-5.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',6) }}">อาคาร ชัยชนะ <img src="{{ asset('themes/default/assets/images/plan/floor-6.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',7) }}">อาคาร ดวงดี <img src="{{ asset('themes/default/assets/images/plan/floor-7.jpg') }}" ></a>
+    <a href="{{ action('HomeController@floor_detail',8) }}">อาคาร ทองแท้ <img src="{{ asset('themes/default/assets/images/plan/floor-8.jpg') }}" ></a>
 </div>
